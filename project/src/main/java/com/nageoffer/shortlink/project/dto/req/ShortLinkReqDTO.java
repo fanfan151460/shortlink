459ea7c45ae2548b78cb92ffdd518c653a5a4e53
FanFanFan <1514606651@qq.com>
2026-06-09 16:01:45 +0800

    日期类型优化

--- project/src/main/java/com/nageoffer/shortlink/project/dto/req/ShortLinkReqDTO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/req/ShortLinkReqDTO.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 public class ShortLinkReqDTO implements Serializable {
@@ -32,7 +32,7 @@ public class ShortLinkReqDTO implements Serializable {
     /**
      * 有效期
      */
-    private LocalDateTime validDate;
+    private LocalDate validDate;
 
     /**
      * 描述
