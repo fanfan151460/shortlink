85be11ce44faf5b0a9a3a545a0b4a3d6cd799894
FanFanFan <1514606651@qq.com>
2026-07-01 17:32:09 +0800

    过滤器注解配置

--- project/src/main/java/com/nageoffer/shortlink/project/ShortLinkProjectApplication.java
+++ project/src/main/java/com/nageoffer/shortlink/project/ShortLinkProjectApplication.java
@@ -3,9 +3,11 @@ package com.nageoffer.shortlink.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.nageoffer.shortlink.project.dao.mapper")
+@ServletComponentScan(basePackages = "com.nageoffer.shortlink.project.common.web")
 public class ShortLinkProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkProjectApplication.class, args);
