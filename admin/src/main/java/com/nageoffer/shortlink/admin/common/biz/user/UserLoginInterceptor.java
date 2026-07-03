de337c4cf9de725c7019918b80dd8a573f3e86ba
FanFanFan <1514606651@qq.com>
2026-07-03 16:53:51 +0800

    网关验证引入

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserLoginInterceptor.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserLoginInterceptor.java
@@ -1,16 +1,11 @@
 package com.nageoffer.shortlink.admin.common.biz.user;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.json.JSONUtil;
-import com.nageoffer.shortlink.admin.dto.req.UserLoginDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.LOGIN;
 
 @Component
 @RequiredArgsConstructor
@@ -21,11 +16,8 @@ public class UserLoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String username = request.getHeader("username");
         String token = request.getHeader("token");
-        Object loginDTO = stringRedisTemplate.opsForHash().get(LOGIN + username, token);
-        UserLoginDTO userLoginDTO = JSONUtil.toBean((String) loginDTO, UserLoginDTO.class);
-        UserInfoDTO userInfoDTO = BeanUtil.copyProperties(userLoginDTO, UserInfoDTO.class, "password");
         // 保存用户上下文
-        UserContext.setUser(userInfoDTO);
+        UserContext.setUser(new UserInfoDTO().setUsername(username).setToken(token));
         return true;
     }
 }
