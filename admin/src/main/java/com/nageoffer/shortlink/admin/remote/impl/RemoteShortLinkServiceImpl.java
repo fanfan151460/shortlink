49b77a1fd042e676968558b8258445987d691314
FanFanFan <1514606651@qq.com>
2026-07-03 23:54:18 +0800

    统一异常响应

--- admin/src/main/java/com/nageoffer/shortlink/admin/remote/impl/RemoteShortLinkServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/impl/RemoteShortLinkServiceImpl.java
@@ -1,8 +1,8 @@
 package com.nageoffer.shortlink.admin.remote.impl;
 
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
-import com.nageoffer.shortlink.admin.remote.ProjectFeignClient;
 import com.nageoffer.shortlink.admin.remote.IRemoteShortLinkService;
+import com.nageoffer.shortlink.admin.remote.ProjectFeignClient;
 import com.nageoffer.shortlink.admin.remote.dto.req.LinkPageReqDTO;
 import com.nageoffer.shortlink.admin.remote.dto.req.RecycleDTO;
 import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkReqDTO;
@@ -23,8 +23,8 @@ public class RemoteShortLinkServiceImpl implements IRemoteShortLinkService {
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkReqDTO reqDTO) {
         Result<ShortLinkCreateRespDTO> result = projectFeignClient.createShortLink(reqDTO);
-        if (result == null || !result.isSuccess()) {
-            throw new RuntimeException("远程创建短链接失败");
+        if (!result.isSuccess()) {
+            throw new RuntimeException(result.getMessage());
         }
         return result.getData();
     }
@@ -37,8 +37,8 @@ public class RemoteShortLinkServiceImpl implements IRemoteShortLinkService {
                 linkPageReqDTO.getSize(),
                 linkPageReqDTO.getOrderFlag()
         );
-        if (result == null || !result.isSuccess()) {
-            throw new RuntimeException("远程分页查询短链接失败");
+        if (!result.isSuccess()) {
+            throw new RuntimeException(result.getMessage());
         }
         return result.getData();
     }
@@ -46,16 +46,16 @@ public class RemoteShortLinkServiceImpl implements IRemoteShortLinkService {
     @Override
     public void updateShortLink(ShortLinkUpReqDTO reqDTO) {
         Result<Void> result = projectFeignClient.updateShortLink(reqDTO);
-        if (result == null || !result.isSuccess()) {
-            throw new RuntimeException("远程更新短链接失败");
+        if (!result.isSuccess()) {
+            throw new RuntimeException(result.getMessage());
         }
     }
 
     @Override
     public void removeShortLink(RecycleDTO recycleDTO) {
         Result<Void> result = projectFeignClient.removeShortLink(recycleDTO);
-        if (result == null || !result.isSuccess()) {
-            throw new RuntimeException("远程删除短链接失败");
+        if (!result.isSuccess()) {
+            throw new RuntimeException(result.getMessage());
         }
     }
 
